6c9d37ac8bd47e5b16a7d1452b547c5281d1fc6b
Adarsh Kumar <adarshkumar245@gmail.com>
2021-06-05 09:51:00 +0530

    edit script.js

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-console.log("hello world");
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+
+const header = document.querySelector("#header");
+
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result > p");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
+
 
 const rps = ["Rock", "Paper", "Scissors"];
 
 function computerPlay() {
     return rps[Math.floor(Math.random()*rps.length)];
 }
+
+let computerSelec = "";
 
 let playerScore = 0;
 let computerScore = 0;
@@ -16,44 +28,71 @@ function playRound(playerSelection, computerSelection) {
     }else if(playerSelection.toUpperCase()==="ROCK"){
          if(computerSelection.toUpperCase()==="PAPER"){
              computerScore++;
+             computerScore_span.textContent = `${computerScore}`;
+             gameStatus(playerScore, computerScore); 
              return "You Lose! Paper beat Rock";
          }else if(computerSelection.toUpperCase()==="SCISSORS"){
              playerScore++;
+             userScore_span.textContent = `${playerScore}`;
+             gameStatus(playerScore, computerScore); 
              return "You Win! Rock Beat Scissors";
          }  
     }else if(playerSelection.toUpperCase()==="PAPER"){
          if(computerSelection.toUpperCase()==="ROCK"){
              playerScore++;
+             userScore_span.textContent = `${playerScore}`;
+             gameStatus(playerScore, computerScore); 
              return "You Win! Paper beat Rock";
          }else if(computerSelection.toUpperCase()==="SCISSORS"){
              computerScore++;
+             computerScore_span.textContent = `${computerScore}`;
+             gameStatus(playerScore, computerScore); 
              return "You Lose! Scissors Beat Paper";
          }  
     }else if(playerSelection.toUpperCase()==="SCISSORS"){
          if(computerSelection.toUpperCase()==="PAPER"){
              playerScore++;
+             userScore_span.textContent = `${playerScore}`;
+             gameStatus(playerScore, computerScore); 
              return "You Win! Scissors beat Paper";
          }else if(computerSelection.toUpperCase()==="ROCK"){
              computerScore++;
+             computerScore_span.textContent = `${computerScore}`;
+             gameStatus(playerScore, computerScore); 
              return "You Lose! Rock Beats Scissors";
          }  
     }
 }
 
-function game(){
-  for(let i = 1; i <=5; i++){
-      let playerSelect = prompt("Rock,Paper or Scissors!!!");
-      let computerSelect = computerPlay();
-      console.log(playRound(playerSelect,computerSelect));    
-  }
-  if(playerScore>computerScore){
-      console.log("Congratualtions!!!You win");
-  }else if(playerScore<computerScore){
-      console.log("Computer Wins!!!");
-  }else{
-      console.log("Nobody Won!!!");
-  }
-
-}
-
-game();
+    function gameStatus(pScore,cScore) {
+    if(pScore>cScore&&pScore===5){
+    header.textContent = "Player Wins!!!!";
+    header.classList.add("green");
+    rock_div.removeEventListener("click", game);
+   }else if(cScore>pScore && cScore===5){
+     header.textContent = ":( Computer Wins :(";
+     header.classList.add("red");
+     rock_div.removeEventListener("click", game);
+     paper_div.removeEventListener("click", game);
+     scissors_div.removeEventListener("click", game);
+ }
+    }
+    
+    function game(e) {
+        console.log(e.target.id);
+        computerSelec = computerPlay();
+        let result = playRound(e.target.id,computerSelec);
+        result_div.textContent = result;
+    }
+    
+    rock_div.addEventListener("click", game);
+    // rock_div.addEventListener("click", function(){
+    //     computerSelec = computerPlay();
+    //     let result = playRound("rock",computerSelec);
+    //     result_div.textContent = result;
+    //     gameStatus(playerScore, computerScore);        
+    // });
+    
+    paper_div.addEventListener("click", game);
+    
+    scissors_div.addEventListener("click", game);  
